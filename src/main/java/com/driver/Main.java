b450ed6cf2ac33e81529647b8ca6f520b9f8e6fb
siddhartha107 <mishrasiddharth107@gmail.com>
2023-01-17 16:18:23 +0530

    solution2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,15 +5,17 @@ public class Main {
         B b = new B();
         b.meth();
     }
-}
-class A{
-    public String meth(){
-        return "Invoking method from class A";
+
+   public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
     }
-}
-class B extends A{
-   @Override
-    public String meth(){
-       return "Method is overridden in Extendend class B";
-   }
+
+    public static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
 }
